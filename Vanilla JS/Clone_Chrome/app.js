1391b99eb5151df4ac0ee3be5077de35f6f0b10e
Choalstn <choalstn@gmail.com>
2022-11-30 00:34:08 +0900

    이벤트 발생 (2) - 다양한 이벤트 속성

--- Vanilla JS/Clone_Chrome/app.js
+++ Vanilla JS/Clone_Chrome/app.js
@@ -1,3 +1,7 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLHeadingElement
+// 다양한 이벤트 속성들 확인할 수 있는 곳 
+
+
 const title = document.querySelector(".hello h1"); 
 
 console.dir(title);
@@ -6,17 +10,40 @@ function handleTitleClick() {
     title.style.color = 'blue';
 }
 
-//handleTitleClick() -> () 붙이면 클릭이 되기 전에도 실행 
-title.addEventListener("click", handleTitleClick);
-
 function handleMouseEnter() {
     title.innerText = 'Mouse is here.';
 }
-
-title.addEventListener("mouseenter", handleMouseEnter);
 
 function handleMouseLeave() {
     title.innerText = 'Mouse is gone.';
 }
 
+//handleTitleClick() -> () 붙이면 클릭이 되기 전에도 실행 
+//title.addEventListener("click", handleTitleClick);
+title.onclick = handleTitleClick;
+
+title.addEventListener("mouseenter", handleMouseEnter);
 title.addEventListener("mouseleave", handleMouseLeave);
+
+
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+    alert("copier!");
+}
+
+function handleWindowOffline() {
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+    alert("All Good");
+}
+
+window.addEventListener('resize', handleWindowResize);
+window.addEventListener('copy', handleWindowCopy);
+window.addEventListener('offline', handleWindowOffline);
+window.addEventListener('online', handleWindowOnline);

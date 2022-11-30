219d80e6ebbaa5c981fc9ec3496ef61000aa1524
Choalstn <choalstn@gmail.com>
2022-11-30 09:57:07 +0900

    CSS in JS (1) - JS  파일에서 요소 클릭 시 색깔 바뀌고 돌아오게

--- Vanilla JS/Clone_Chrome/app.js
+++ Vanilla JS/Clone_Chrome/app.js
@@ -1,49 +1,15 @@
-// https://developer.mozilla.org/en-US/docs/Web/API/HTMLHeadingElement
-// 다양한 이벤트 속성들 확인할 수 있는 곳 
-
-
-const title = document.querySelector(".hello h1"); 
-
-console.dir(title);
+const h1 = document.querySelector(".hello h1"); 
 
 function handleTitleClick() {
-    title.style.color = 'blue';
+    const currentColor = h1.style.color;
+    let newColor;
+
+    if(currentColor === "blue") {
+        newColor = "tomato";
+    } else {
+        currentColor = "blue";
+    }
+    h1.style.color = newColor;
 }
 
-function handleMouseEnter() {
-    title.innerText = 'Mouse is here.';
-}
-
-function handleMouseLeave() {
-    title.innerText = 'Mouse is gone.';
-}
-
-//handleTitleClick() -> () 붙이면 클릭이 되기 전에도 실행 
-//title.addEventListener("click", handleTitleClick);
-title.onclick = handleTitleClick;
-
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-
-
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-    alert("copier!");
-}
-
-function handleWindowOffline() {
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-    alert("All Good");
-}
-
-window.addEventListener('resize', handleWindowResize);
-window.addEventListener('copy', handleWindowCopy);
-window.addEventListener('offline', handleWindowOffline);
-window.addEventListener('online', handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);

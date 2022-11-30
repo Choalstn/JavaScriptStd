643479f83f03a74b544b49ceec84722780790208
Choalstn <choalstn@gmail.com>
2022-11-30 10:22:09 +0900

    CSS in JS (2) - JS파일에서 style 분리 & 클릭 시 class 추가

--- Vanilla JS/Clone_Chrome/app.js
+++ Vanilla JS/Clone_Chrome/app.js
@@ -1,15 +1,18 @@
 const h1 = document.querySelector(".hello h1"); 
 
 function handleTitleClick() {
-    const currentColor = h1.style.color;
-    let newColor;
+    //h1에 class="active"를 추가해줌으로써 글자 색상을 tomato로 바꿔줌
+    //js에서 직접적으로 style을 건드리지 않게 된 것
+    //단점 : h1에 기존 클래스가 존재하더라도 신경쓰지 않고 바꿔버림
+    
+    //변수로 지정해줌으로써 에러 발생률을 낮춤 
+    const clickedClass = "clicked"
 
-    if(currentColor === "blue") {
-        newColor = "tomato";
+    if(h1.className === clickedClass) {
+        h1.className = "";
     } else {
-        currentColor = "blue";
+        h1.className = clickedClass;
     }
-    h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleTitleClick);
